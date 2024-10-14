7b8e2e4d0b364a52685ae5573ef80bf6815791d2
Jadylyn31 <jadylyndolorico3@gmail.com>
2024-10-14 11:47:19 +0800

    Add files via upload

--- TASK_1.py
+++ TASK_1.py
@@ -1,13 +1,15 @@
-user = input("Put your grade:")
+grade = int(input("Put your grade:"))
+
 
 def check_grade(grade):
-    if grade > 80-100:
-     return "You did a great job!"
-    elif grade < 70-79:
-     return "Keep it up!"
-    elif grade < 60-69:
-     return "You passed, but there's room for improvement."
-    elif grade < Below 50:
-     return "Better luck next time"
+    if grade >= 80 and grade <= 100:
+        return "You did a great job!"
+    elif grade >= 70 and grade <=79:
+        return "Keep it up!"
+    elif grade >= 60 and grade <= 69:
+        return "You passed, but there's room for improvement."
+    elif grade >= 50:
+        return "Better luck next time"
+   
 
-print(check_grade(10))
+print(check_grade(grade))
